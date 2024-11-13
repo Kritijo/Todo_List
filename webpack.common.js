7bce7bebacbacfa6b52cb262e268ee98f267f224
KRITI JOSHI <kritijoshi@KRITIs-MacBook-Pro.local>
2024-11-13 14:54:05 +0530

    Setup entry and output configuration

--- webpack.common.js
+++ webpack.common.js
@@ -1,1 +1,13 @@
 
+const path = require("path");
+
+module.exports = {
+  entry: {
+    app : "./src/js_files/index.js",
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
