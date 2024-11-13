339b6c4f86ad2999ed881b43eeb11340d08ae7f1
KRITI JOSHI <kritijoshi@KRITIs-MacBook-Pro.local>
2024-11-13 15:35:12 +0530

    Add htmlWebpackPlugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,16 @@
-
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     app : "./src/js_files/index.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      title: 'Production',
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, "dist"),
