f321fd98f158f3a58b6e2e77951af9b2768b286c
KRITI JOSHI <kritijoshi@KRITIs-MacBook-Pro.local>
2024-11-24 16:28:01 +0530

    Add event listeners to create and edit todo

--- src/js_files/index.js
+++ src/js_files/index.js
@@ -1,12 +1,23 @@
 import "../styles.css";
 import {displayMenu, disableMenu} from "./menu";
+import {createTodo, editTodo} from "./todos";
+
 let toggle = true;
 
 const displayBoard = document.querySelector(".displayBoard");
 const todoSection = document.querySelector(".todo-section");
 let menuToggle = document.querySelector("nav div");
 
-// let bttn = document.querySelector("button");
+let bttn = document.querySelector("button");
+bttn.addEventListener("click",()=>{
+    createTodo();
+});
+
+document.addEventListener("change", (e)=>{
+        if(e.target.classList.contains('todoElem')){
+            editTodo(e.target);
+        }
+});
 
 displayMenu();
 menuToggle.addEventListener("click",()=>{
