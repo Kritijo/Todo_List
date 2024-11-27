284f1afd4b065a28b67dcd0be36a1957251c1794
KRITI JOSHI <kritijoshi@KRITIs-MacBook-Pro.local>
2024-11-27 14:03:18 +0530

    Implement setupMenu() to listen to menu buttons

--- src/js_files/menu.js
+++ src/js_files/menu.js
@@ -1,12 +1,17 @@
+import {displayBoard} from "./index.js"
+import { filterTodos } from "./todos.js";
+
 let menu = document.querySelector(".menu");
 
 export function displayMenu(){
     menu.className = "menu";
+    displayBoard.style.gridTemplateColumns = "30vw 1fr"
     let arr = ["All tasks", "Completed", "Upcoming", "Projects"];
+    let classes = ["all-tasks", "completed-tasks", "incomplete-tasks"]
 
     for(let i=0; i<4; i++){
         let menuItems = document.createElement("div");
-        menuItems.className = "menu-items";
+        menuItems.classList.add("menu-items", `${classes[i]}`);
         menuItems.textContent = arr[i];
         menu.append(menuItems);
     }
@@ -14,5 +19,16 @@ export function displayMenu(){
 
 export function disableMenu(){
     menu.textContent = '';
+    displayBoard.style.gridTemplateColumns = "1fr"
     menu.classList.remove("menu");
 }
+
+export function setupMenu() {
+    const allTasksButton = document.querySelector(".all-tasks");
+    const completedTasksButton = document.querySelector(".completed-tasks");
+    const incompleteTasksButton = document.querySelector(".incomplete-tasks");
+
+    allTasksButton.addEventListener("click", () => filterTodos("all"));
+    completedTasksButton.addEventListener("click", () => filterTodos("completed"));
+    incompleteTasksButton.addEventListener("click", () => filterTodos("incomplete"));
+};
